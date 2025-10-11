c64f43958961ef8d14fb9d99619a8142639ec2bb
aaAndrewr <zmyshkoandrew@gmail.com>
2025-10-11 12:46:44 +0300

    hw-12-about

--- module-2-hw/hw-12/js/main.js
+++ module-2-hw/hw-12/js/main.js
@@ -1,30 +1,3 @@
-// (function() {
-// const burgerIcon = document.querySelector('.burger-icon')
-// const burgerNavLink = document.querySelectorAll('.nav__link')
-
-// console.log(document.body)
-// console.log(burgerIcon)
-// console.log(burgerNavLink)
-
-// burgerIcon.addEventListener('click', () => {
-//     if(document.body.classList.contains('body--opened-menu')) {
-//         document.body.classList.remove('body--opened-menu')
-//     } else {
-//         document.body.classList.add('body--opened-menu')
-//     }
-// })
-
-// for(let i = 0; i < burgerNavLink.length; i++) {
-//     burgerNavLink[i].addEventListener("click", function() {
-//         if(document.body.classList.contains('body--opened-menu')) {
-//             document.body.classList.remove('body--opened-menu')
-//         } else {
-//             document.body.classList.add('body--opened-menu')
-//         }
-//     }) 
-// }
-// })()
-
 (function() {
     document.addEventListener('click', burgerInit)
 
@@ -42,4 +15,49 @@
             document.body.classList.remove('body--opened-menu')
         }
     }
+
+    ///////////////////////////////modal////////////////////////////////////////
+
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__controls-button')
+
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+        document.querySelector('.modal').classList.toggle('modal--opened')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+            document.querySelector('.modal').classList.remove('modal--opened')
+        }
+
+    }
 })()
+
+
+    // мой вариант модального окна
+    // const modalClose = document.querySelector('.modal__cancel')
+    // const modalOpen = document.querySelector('.about__controls-button')
+    
+    // console.log(modalOpen)
+    // console.log(modalClose)
+    // console.log(document.querySelector('.modal'))
+    
+    // modalOpen.addEventListener('click', () => {
+    //     document.body.classList.add('body--opened-modal')
+    //     document.querySelector('.modal').classList.add('modal--opened')
+    // })
+    
+    // modalClose.addEventListener('click', () => {
+    //     document.body.classList.remove('body--opened-modal')
+    //     document.querySelector('.modal').classList.remove('modal--opened')
+    // })
